5146053acbccfe5651886319032da4c073c9715a
Fern_wanwisa <66070179@kmitl.ac.th>
2024-09-20 20:36:40 +0700

    add head status

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
 
 function statusPage() {
   return (
     <div className='laptop:aspect-[16/8.4] bg-[--neutrals-color]'>
-
+      <div className='grid grid-cols-2 pt-32'>
+        <div className='pl-32 flex'>
+          <h1 className = "text-3xl text-[#5865F2] font-bold ">รายการบอร์ดเกม</h1>
+        </div>
+          
+        <div className='pl-32 flex items-end'>
+          <input type="text" placeholder='searh boardgame' className='text-[#000000] mt-1 px-3 py-2 border-b shadow-sm border-slate-300 w-96 rounded-lg sm:text-sm focus:outline-none focus:border-[--primary-color] transition focus:duration-200'/>
+        </div>
+      </div>
     </div>
   )
 }
