080260d28288f0af60fb8830a0cfc03daa7d62a1
NiflheimZ-P <siwaphat.gundej@gmail.com>
2024-09-24 23:33:54 +0700

    fix gradient Button Color on Food_Page
    change hover from Component-Lib-Button to hover in Component-foodCard

--- src/app/components/foodCard.tsx
+++ src/app/components/foodCard.tsx
@@ -7,7 +7,7 @@ export default class foodCard extends React.Component {
   render() {
     return (
       <div className="max-w-full min-w-[1000] shadow-2xl mt-2">
-        <Button className="grid grid-col-2 bg-[#292929] hover:border-[--primary-color] border-transparent border-2 h-[400px] w-full rounded-2xl">
+        <Button className="grid grid-col-2 bg-[#292929]  hover:bg-[--neutral-color] hover:border-[--primary-color] border-transparent border-2 h-[400px] w-full rounded-2xl">
           {/* inbox picture Set */}
           <div className=" w-64 mx-auto top-0 mb-3">
             <div className="bg-[url('../assets/boardgame.jpeg')] object-scale-down h-48 w-79 rounded-2xl overflow-hidden shadow-md"></div>
