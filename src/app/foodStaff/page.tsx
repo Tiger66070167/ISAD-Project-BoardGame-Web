f485b797154c37a4dc4dbb512eab009093adf34a
Tiger66070167 <139055235+Tiger66070167@users.noreply.github.com>
2024-09-28 16:30:05 +0700

    FixFoodStaff

--- src/app/foodStaff/page.tsx
+++ src/app/foodStaff/page.tsx
@@ -1,87 +1,14 @@
-import React from "react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Chip, Tooltip, ChipProps, getKeyValue} from "@nextui-org/react";
-import {EditIcon} from "./EditIcon";
-import {DeleteIcon} from "./DeleteIcon";
-import {EyeIcon} from "./EyeIcon";
-import {columns, users} from "./data";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
 
-const statusColorMap: Record<string, ChipProps["color"]>  = {
-    active: "success",
-    paused: "danger",
-    vacation: "warning",
-};
-
-    type User = typeof users[0];
-
-    export default function App() {
-    const renderCell = React.useCallback((user: User, columnKey: React.Key) => {
-        const cellValue = user[columnKey as keyof User];
-
-        switch (columnKey) {
-        case "name":
-            return (
-            <User
-                avatarProps={{radius: "lg", src: user.avatar}}
-                description={user.email}
-                name={cellValue}
-            >
-                {user.email}
-            </User>
-            );
-        case "role":
-            return (
-            <div className="flex flex-col">
-                <p className="text-bold text-sm capitalize">{cellValue}</p>
-                <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
-            </div>
-            );
-        case "status":
-            return (
-            <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
-                {cellValue}
-            </Chip>
-            );
-        case "actions":
-            return (
-            <div className="relative flex items-center gap-2">
-                <Tooltip content="Details">
-                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                    <EyeIcon />
-                </span>
-                </Tooltip>
-                <Tooltip content="Edit user">
-                <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                    <EditIcon />
-                </span>
-                </Tooltip>
-                <Tooltip color="danger" content="Delete user">
-                <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                    <DeleteIcon />
-                </span>
-                </Tooltip>
-            </div>
-            );
-        default:
-            return cellValue;
-        }
-    }, []);
-
-return (
-    <Table aria-label="Example table with custom cells">
-        <TableHeader columns={columns}>
-            {(column: any) => (
-            <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
-                {column.name}
-            </TableColumn>
-            )}
-        </TableHeader>
-        <TableBody items={users}>
-            {(item: any) => (
-            <TableRow key={item.id}>
-                {(columnKey: any) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-            </TableRow>
-            )}
-        </TableBody>
-        </Table>
-    );
+export default function App() {
+  return (
+    <Table aria-label="Example empty table">
+      <TableHeader>
+        <TableColumn>NAME</TableColumn>
+        <TableColumn>ROLE</TableColumn>
+        <TableColumn>STATUS</TableColumn>
+      </TableHeader>
+      <TableBody emptyContent={"No rows to display."}>{[]}</TableBody>
+    </Table>
+  );
 }
