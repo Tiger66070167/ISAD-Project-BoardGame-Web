bd3944078be30506d17ea5ff4d935fb10bdbee6e
Tiger66070167 <139055235+Tiger66070167@users.noreply.github.com>
2024-09-28 16:41:02 +0700

    fix tailwind not defind

--- src/app/foodStaff/page.tsx
+++ src/app/foodStaff/page.tsx
@@ -1,14 +1,14 @@
 import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@nextui-org/react";
 
 export default function App() {
-  return (
-    <Table aria-label="Example empty table">
-      <TableHeader>
-        <TableColumn>NAME</TableColumn>
-        <TableColumn>ROLE</TableColumn>
-        <TableColumn>STATUS</TableColumn>
-      </TableHeader>
-      <TableBody emptyContent={"No rows to display."}>{[]}</TableBody>
-    </Table>
-  );
+    return (
+        <Table aria-label="Example empty table">
+        <TableHeader>
+            <TableColumn>NAME</TableColumn>
+            <TableColumn>ROLE</TableColumn>
+            <TableColumn>STATUS</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent={"No rows to display."}>{[]}</TableBody>
+        </Table>
+    );
 }
